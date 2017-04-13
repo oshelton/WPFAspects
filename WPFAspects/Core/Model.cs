1a83b1b5d4f9b8d8beb57e92b4bf5859af2b7f1f
Owen Shelton <jowenshelton@gmail.com>
2017-04-12 20:28:06 -0400

    Issues #1, #2
    Added base data and view model classes and a little bit of a skeleton of the validation classes including base validator and rule classes.  They are currently just barely integrated into the model classes.
    
    This finishes issue #1; though modifications will almost certainly be made in the future to it.

---
+++ WPFAspects/Core/Model.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Copyright 2017 Jack owen Shelton
+ * Licensed under the terms of the MIT license.
+ * Part of the WPFAspects project. 
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFAspects.Core
+{
+    /// <summary>
+    /// Base class for Data and View model classes.
+    /// </summary>
+    public abstract class Model : INotifyDataErrorInfo, INotifyPropertyChanging, INotifyPropertyChanged
+    {
+        Model()
+        {
+            Validator = GetValidator();
+        }
+
+        #region Validation related items.
+        public bool HasErrors => Validator?.HasErrors ?? false;
+        //TODO: Implement me! I should not be abstract.  I need stuff from the Validator class that doesn't exist yet though.
+        public System.Collections.IEnumerable GetErrors(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        /// <summary>
+        /// Subclasses should override this method in order to provide an instance of a Validator object appropriate
+        /// to the type.
+        /// </summary>
+        /// <remarks>
+        /// Null can be returned for objects that don't really need validation.
+        /// </remarks>
+        protected virtual Validation.Validator GetValidator() { return null; }
+        public Validation.Validator Validator { get; protected set; }
+        #endregion
+
+        #region INotifyPropertyChanging related items.
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        protected virtual void OnThisPropertyChanging([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        }
+        #endregion
+
+        #region INotifyPropertyChanged related items.
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected virtual void OnThisPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
---
+++ WPFAspects/Validation/Validator.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Copyright 2017 Jack owen Shelton
+ * Licensed under the terms of the MIT license.
+ * Part of the WPFAspects project. 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFAspects.Validation
+{
+    /// <summary>
+    /// Base class for all object validators.  Implements just the common validation properties, events, and methods.
+    /// </summary>
+    /// <remarks>
+    /// This class should not be inherited from in client code.  Instead use the generic subclass of this class.
+    /// </remarks>
+    public abstract class Validator
+    {
+        ///Get whether or not the validated object has any validation errors.
+        public bool HasErrors { get; protected set; }
+    }
+
+    /// <summary>
+    /// Base class for object validators.  This is what you want to subclass in client code.
+    /// </summary>
+    /// <typeparam name="T">The type this validator validates.</typeparam>
+    public abstract class Validator<T> : Validator
+    {
+
+    }
+}
