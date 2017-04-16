871264ed1b8e2ca8d6aa48a9be2c444df8d7f48e
Owen Shelton <jowenshelton@gmail.com>
2017-04-16 19:01:45 -0400

    Added initial PropertyGetter tests.  Several more still need to be written for full coverage.
    For issue #4.

---
+++ UtilTests/PropertyGetterTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WPFAspects.Utils;
+
+namespace UtilTests
+{
+    [TestClass]
+    public class PropertyGetterTests
+    {
+        private class TestObject
+        {
+            public static bool StaticVarOne { get; set; }
+            private static bool StaticVarTwo { get; set; } = true;
+
+            public int VarOne { get; set; }
+            internal int VarTwo { get; set; } = 1;
+            protected int VarThree { get; set; } = 2;
+            private int VarFour { get; set; } = 3;
+        }
+
+        ///Test default PropertyGetter.GetProperties behavior; should only return the VarOne property.
+        [TestMethod]
+        public void TestDefaultBindingFlags()
+        {
+            var testObject = new TestObject();
+            var properties = testObject.GetProperties();
+
+            Assert.AreEqual(1, properties.Count());
+            Assert.AreEqual("VarOne", properties.First().Name);
+        }
+    }
+}
