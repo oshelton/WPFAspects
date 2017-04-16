c7b179f51f94e36a56e6388927d98d3ace246d58
Owen Shelton <jowenshelton@gmail.com>
2017-04-16 18:28:18 -0400

    Added PropertyGetter util class.
    For issue #4.
    
    PropertyGetter class provides simple methods for retrieving the properties of a type, getting the value of a property, and setting the value of a property.

---
+++ WPFAspects/Utils/PropertyGetter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFAspects.Utils
+{
+    ///Class for retrieving property information for a particular type.
+    ///The framework itself seems to do some caching with regards to this stuff so I am not going to try to outdo it.
+    ///<remarks>
+    ///Reflection is costly, don't use it if you have a choice.
+    ///</remarks>
+    public static class PropertyGetter
+    {
+        ///Get all properties of an object matching the passed in binding flags.
+        public static IEnumerable<PropertyInfo> GetProperties(this object obj, BindingFlags flags = BindingFlags.Default)
+        {
+            return obj.GetType().GetProperties(flags);
+        }
+
+        ///Get the value of a specific property of an object.
+        public static T GetPropertyValue<T>(this object obj, string propertyName)
+        {
+            return (T)obj.GetType().GetProperty(propertyName).GetValue(obj);
+        }
+
+        ///Set the value of a specific property of an object.
+        public static void SetPropertyValue<T>(this object obj, string propertyName, T value)
+        {
+            obj.GetType().GetProperty(propertyName).SetValue(obj, value);
+        }
+    }
+}
