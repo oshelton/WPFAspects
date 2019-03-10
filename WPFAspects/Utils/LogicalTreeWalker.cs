f1a12c1e1a98979efc4cfa5a55c1b7b71e3331e5
Owen Shelton <jowenshelton@gmail.com>
2019-03-10 18:09:38 -0400

    Added logical tree walker and made some minor improvements to the visual tree walker. #4

--- WPFAspects/Utils/LogicalTreeWalker.cs
+++ WPFAspects/Utils/LogicalTreeWalker.cs
@@ -4,28 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Media;
 
 namespace WPFAspects.Utils
 {
-    public static class VisualTreeWalker
+    public static class LogicalTreeWalker
     {
         /// <summary>
-        /// Find and return the first child visual of a specific type. 
+        /// Find a child logical element with the given name.
         /// </summary>
-        /// <param name="recursive">Whether or not the visual tree should be searched recursively.  Defaults to true.</param>
+        /// <param name="childName">The name of the child element to search for.</param>
+        /// <returns>The found element, or null if one is not found.</returns>
+        public static T FindChild<T>(FrameworkElement parent, string childName) where T:FrameworkElement
+        {
+            return LogicalTreeHelper.FindLogicalNode(parent, childName) as T;
+        }
+
+        /// <summary>
+        /// Find a parent logical element with the given name.
+        /// </summary>
+        /// <param name="childName">Name of the parent to search for.</param>
+        /// <remarks>This method is subject to WPF's name scopes https://docs.microsoft.com/en-us/dotnet/framework/wpf/advanced/wpf-xaml-namescopes?view=netframework-4.7.2.</remarks>
+        /// <returns>Parent with the given name.</returns>
+        public static T FindAncestor<T>(FrameworkElement child, string childName) where T:FrameworkElement
+        {
+            return child.FindName(childName) as T;
+        }
+
+        /// <summary>
+        /// Find the first child logical element of a given type.
+        /// </summary>
+        /// <param name="recursive">Whether or not the logical tree should be searched recursively.  Defaults to true.</param>
         /// <param name="depthLimit">How deep the search should go. Null results in an unbounded search.</param>
         /// <remarks>
         ///     The depthLimit parameter can be used to make the search end prematurely if a child is not found at the expected depth.
         ///     If recursive is false depthLimit does nothing.
         /// </remarks>
         /// <returns>The child found or null.</returns>
-        public static T FindFirstChildOfType<T>(DependencyObject parent, bool recursive = true, uint? depthLimit = null) where T: DependencyObject
+        public static T FindFirstChildOfType<T>(FrameworkElement parent, bool recursive = true, uint? depthLimit = null) where T:FrameworkElement
         {
-            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); ++i)
+            foreach (var child in LogicalTreeHelper.GetChildren(parent).Cast<FrameworkElement>())
             {
-                var child = VisualTreeHelper.GetChild(parent, i) as DependencyObject;
-
                 if (child is T)
                     return child as T;
                 else if (recursive && (depthLimit == null || depthLimit > 0))
@@ -40,22 +58,20 @@ namespace WPFAspects.Utils
         }
 
         /// <summary>
-        /// Find all children of a specific type of a passed in object.
+        /// Find all logical children of a given type.
         /// </summary>
-        /// <param name="recursive">Whether or not the visual tree should be searched recursively.  Defaults to true.</param>
+        /// <param name="recursive">Whether or not the logical tree should be searched recursively.  Defaults to true.</param>
         /// <param name="depthLimit">How deep the search should go. Null results in an unbounded search.</param>
         /// <remarks>
         ///     The depthLimit parameter can be used to make the search end prematurely if a child is not found at the expected depth.
         ///     If recursive is false depthLimit does nothing.
         /// </remarks>
         /// <returns>A collection containing any children found of the appropriate type, or an empty collection.</returns>
-        public static IReadOnlyList<T> FindChildrenOfType<T>(DependencyObject parent, bool recursive = true, uint? depthLimit = null) where T: DependencyObject
+        public static IReadOnlyList<T> FindChildrenOfType<T>(FrameworkElement parent, bool recursive = true, uint? depthLimit = null) where T : FrameworkElement
         {
             List<T> children = new List<T>();
-            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); ++i)
+            foreach (var child in LogicalTreeHelper.GetChildren(parent).Cast<FrameworkElement>())
             {
-                var child = VisualTreeHelper.GetChild(parent, i) as DependencyObject;
-
                 if (child is T)
                     children.Add(child as T);
                 else if (recursive && (depthLimit == null || depthLimit > 0))
@@ -66,16 +82,16 @@ namespace WPFAspects.Utils
         }
 
         /// <summary>
-        /// Find the visual ancestor of the object with the specified type.
+        /// Find the logical ancestor of the object with the specified type.
         /// </summary>
         /// <param name="heightLimit">How high the search should go. Null results in an unbounded search.</param>
         /// <remarks>
         ///     The heightLimit parameter can be used to make the search end prematurely if a parent is not found at the expected height.
         /// </remarks>
         /// <returns>The found ancestor or null.</returns>
-        public static T FindFirstAncestorOfType<T>(DependencyObject child, uint? heightLimit = null) where T: DependencyObject
+        public static T FindFirstAncestorOfType<T>(FrameworkElement child, uint? heightLimit = null) where T : FrameworkElement
         {
-            var parent = VisualTreeHelper.GetParent(child) as DependencyObject;
+            var parent = LogicalTreeHelper.GetParent(child) as FrameworkElement;
 
             if (parent is T)
                 return parent as T;
